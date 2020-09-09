8f402c900c2159a63da2d8a47fefddf572d9f71a
Saul Moreno Abril <93sauu@gmail.com>
2020-09-09 18:11:50 +0200

    wip: map view controller tests

---
+++ FindMyRoute-DemoTests/ModulesTests/Map/MapViewControllerTests.swift
@@ -0,0 +1,42 @@
+//
+//  MapViewControllerTests.swift
+//  FindMyRoute-DemoTests
+//
+//  Created by Saul Moreno Abril on 9/9/20.
+//  Copyright © 2020 Saul Moreno Abril. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RxSwift
+import RxCocoa
+import NSObject_Rx
+import RxBlocking
+import MapKit
+@testable import FindMyRoute_Demo
+
+class MapViewControllerTests: QuickSpec {
+    override func spec() {
+        var view: MapViewController!
+
+        beforeEach {
+            let provider = RestApi(meepProvider: MeepNetworking.stubbingNetworking())
+            let viewModel = MapViewModel(provider: provider)
+            let navigator = Navigator.default
+
+            view = MapViewController(viewModel: viewModel, navigator: navigator)
+            view.loadViewIfNeeded()
+        }
+
+        describe("A map view") {
+            it("sets up the UI") {
+                expect(view.mapView).toNot(beNil())
+                expect(view.mapView?.bounds) != .zero
+            }
+
+            it("binds the view model") {
+
+            }
+        }
+    }
+}
